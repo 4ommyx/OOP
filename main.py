c20cdcfe126d555044e194533f898f1790525a20
4ommyx <nattawutth011@gmail.com>
2023-02-09 23:20:13 +0700

    adapted post

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-class Post(Workshop):
+class Post:
     def __init__(self,id,media_link,poster): 
         self.__id = id
         self.__media_link = media_link
--- post.py
+++ post.py
@@ -1,6 +1,5 @@
-from workshop import Workshop
-class Post(Workshop):
-    def __init__(self,id,media_link,poster): 
+class Post():
+    def __init__(self,id,media_link,poster):
         self.__id = id
         self.__media_link = media_link
         self.__poster = poster 
